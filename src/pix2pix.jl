ec528d8b0c94fffddd6aab7e27ea7bcc57f7a9f4
Shreyas <shreyaskowshik@gmail.com>
2021-07-20 12:58:48 +0530

    Dependencies

--- src/pix2pix.jl
+++ src/pix2pix.jl
@@ -1,5 +1,17 @@
 module pix2pix
 
-
+using CUDAnative
+using CUDAnative:exp,log
+using Images,CuArrays,Flux
+using Flux:@treelike, Tracker
+using Base.Iterators: partition
+using Random
+using Statistics
+using Flux.Tracker:update!
+using BSON: @save
+using Flux:testmode!
+using Distributions:Normal,Uniform
+using JLD
+using Plots
 
 end # module
